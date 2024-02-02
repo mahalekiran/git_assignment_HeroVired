9f5d2710c92cb30c471b006bee3793c1d0f1e5af
Kiran Prakash Mahale <krn1988@gmail.com>
2024-02-02 23:08:19 +0530

    Feature change added

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -13,8 +13,8 @@ class Calculator:
         return a / b
 
     # TODO: Implement the following function to calculate the square root of a number.
-    # def square_root(self, x):
-    # return math.sqrt(x)
+    def square_root(self, x):
+         return math.sqrt(x)
     # You need to uncomment the above function and complete its implementation to add the square root feature.
 
 if __name__ == "__main__":
@@ -28,5 +28,5 @@ if __name__ == "__main__":
         print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
         
         # TODO: Uncomment and test the square root feature.
-        # num3 = 25
-        # print(f"The square root of {num3} = {calculator.square_root(num3)}")
+        num3 = 25
+        print(f"The square root of {num3} = {calculator.square_root(num3)}")
